b57be4f0ec2837232058e09753c92ff94e229473
GrassPlatypus <dreams200260@g.skku.edu>
2025-12-04 18:29:18 +0900

    build

--- move.sh
+++ move.sh
@@ -2,6 +2,8 @@ cp ./out/boot/* /media/`whoami`/boot
 sudo cp -r ./out/rootfs/* /media/`whoami`/rootfs
 sudo cp ./mytee_examples/myta_fb_mmap/host/myta_fb_mmap_no_ta /media/`whoami`/rootfs/bin
 sudo cp ./mytee_examples/myta_fb_write/myta_fb_write_no_ta /media/`whoami`/rootfs/bin
+sudo cp ./mytee_examples/toctou_attack/toctou_victim.ko /media/`whoami`/rootfs/rootfs
+sudo cp ./mytee_examples/toctou_attack/toctou_attacker.ko /media/`whoami`/rootfs/rootfs
 sudo cp ./mytee_examples/tpm_orig/eltt2 /media/`whoami`/rootfs/bin/myta_tpm_no_ta
 cp linux/arch/arm/boot/dts/*.dtb /media/`whoami`/boot
 sync
